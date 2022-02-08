9b37e4fbaf3c2dd33d9ab7960ed63e9710308bea
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-02-08 20:21:54 +0900

    update <img>

--- components/Post.js
+++ components/Post.js
@@ -7,7 +7,7 @@ export default function Post({ post }) {
       {/* <img src={post.frontmatter.cover_image} alt='' className="aspect-[16/9] rounded-2xl" /> */}
 
       <li className="mb-1 block whitespace-nowrap text-ellipsis overflow-hidden">
-        <img src="/arrow_right_black_24dp.svg" className="inline-block" width={16}/>
+        <img src="/arrow_right_black_24dp.svg" alt="" className="inline-block" width="16px" height="16px"/>
         <Link href={`/blog/${post.slug}`}>
           <a className="hover:underline hover:text-newmo-400 visited:text-newmo-300">{post.frontmatter.title}</a>
         </Link>
