578a8f4b01f72135e302e8bb18a54f55a71c9303
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-02-09 01:06:25 +0900

    add sitemap proto

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
-  webpack(config, options) {
+  webpack(config, { isServer }) {
+    if (isServer) {
+      require('./scripts/generate-sitemap.js')
+    }
     config.resolve.alias['@components'] = path.join(__dirname, 'components');
     config.resolve.alias['@utils'] = path.join(__dirname, 'utils');
     return config
