6b20bb91d80f7b4750e11c3eba0bfc446987c797
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-02-13 18:05:27 +0900

    update imageOptimizer

--- utils/ImageOptimizer.ts
+++ utils/ImageOptimizer.ts
@@ -1,9 +1,9 @@
 const sharp = require('sharp');
 const fs = require('fs/promises');
-import {pipeline} from 'node:stream';
-import {promisify} from 'node:util'
+import { pipeline } from 'node:stream';
+import { promisify } from 'node:util'
 import  fetch  from 'node-fetch'
-import {createWriteStream} from 'node:fs';
+import { createWriteStream, existsSync } from 'node:fs';
 const streamPipeline = promisify(pipeline);
 
 type converterProps = {
@@ -11,45 +11,87 @@ type converterProps = {
   name: string
 }
 
-
-const getImageFromWeb:any = async(url :string, index:number) => {
-  const response:any = await fetch(url)
-  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
-
-  const hoge = await streamPipeline(response.body, createWriteStream(`./tmp/octocat${index}.png`));
-  return `/tmp/octocat${index}.png`
+type ImageConverterProps = {
+  url: string,
+  index: number,
+  name: string,
 }
 
 
+const getImageFromWeb = async({
+    url: url,
+    index: index,
+    name: name
+  }: ImageConverterProps) : Promise<string> => {
+  const response:any = await fetch(url);
+  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 
-export default async function converter({images, name}:converterProps) {
-  let output_file_names:string[] = [];
+  await streamPipeline(response.body, createWriteStream(`./tmp/${name}-${index}.png`));
+
+  return `/tmp/${name}-${index}.png`
+}
+
+const existCacheImage = ({
+  url: url,
+  index: index,
+  name: name
+}:ImageConverterProps): boolean => {
+  try {
+    const slash_parsed_url = url?.split('/')?.reverse()[0]
+    const ext = slash_parsed_url?.split('.').reverse()[0];
+    const file = `${process.cwd()}/tmp/${name}-${index}.${ext}`;
+
+    if ( existsSync(file) ) {
+      console.info("Exist Cache Image");
+      return true;
+    }
+  }
+  catch (e) {
+    console.error('Error: ', e)
+  }
+
+  return false;
+}
+
+
+const converter = async({images, name}:converterProps):Promise<string[]> => {
+  let output_file_names:string[] = []
+
   const images_url = images?.map((image:string) => {
     return image.match(/src="https:\/\/i\.gyazo.*?"/)?.toString().slice(5,-1)
   })
 
   if (images_url) {
     for( const [index, image_url] of images_url.entries() ) {
-        console.log("image_url", image_url)
+      const flag = !existCacheImage({
+          name: name,
+          index: index,
+          url: image_url ?? "" })
 
-        const web_image = await getImageFromWeb(image_url, index);
-        const file = await fs.readFile(process.cwd() + web_image)
+      if (!flag) {
+        const fetch_image_path = await getImageFromWeb({
+            url: image_url ?? "",
+            index: index,
+            name: name })
+
+        const file = await fs.readFile(process.cwd() + fetch_image_path);
         const output_file = `images/posts/test/${name}-${index}.avif`
 
         try {
           await sharp(file)
               .resize({ width: 672 })
               .toFormat("avif")
-              .toFile("./public/" +output_file);
-          output_file_names.push(output_file);
+              .toFile("./public/" +output_file)
+          output_file_names.push(output_file)
         }
         catch (e) {
           console.error('Error: ', e)
         }
+      }
     }
   }
 
   return output_file_names
 }
 
-
+export default converter
