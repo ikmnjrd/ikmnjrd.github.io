bfec02f7e2c1c5f491d0888aea9c4c2bf508fdff
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-02-20 23:45:32 +0900

    tmp

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,9 +16,7 @@ export default function about({}) {
       <h1 className="text-3xl mt-8 mb-4">About</h1>
       <div>
         <p>雑多な記録</p>
-        <p>Googleアナリティクス</p>
-        <div>hoge</div>
-        <p>shoge</p>
+        <p>当サイトではGoogle Analyticsを用いて訪問者の情報収集を行っております。</p>
       </div>
     </>
   )
