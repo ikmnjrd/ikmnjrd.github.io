9069ecfb1e6c8456408a3ea69d6ffc48d415e8af
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-03-05 21:53:43 +0900

    add footer waves

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,32 +9,38 @@ type Props = {
 const Footer: FunctionComponent = ({ children }: Props) => {
   const date = new Date();
   return (
-    <footer className="text-center py-5 text-sm text-newmo-300" role="contentinfo">
-      <ul className="flex items-center justify-between max-w-[100px] mx-auto">
-        <li>
-          <a
-            href="https://twitter.com/ikmnjrd"
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-            title="Twitter"
-            className="hover:opacity-50 block"
-          >
-            <SvgIcon type="twitter" size="2.3em" strokeWidth={2.3} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/ikmnjrd"
-            target="_blank"
-            rel="nofollow noopener noreferrer"
-            title="GitHub"
-            className="hover:opacity-50 block"
-          >
-            <SvgIcon type="github" size="2em" strokeWidth={1} />
-          </a>
-        </li>
-      </ul>
-      <p>©︎ikmnjrd - <time dateTime={date.toString()}>{ date.getFullYear() }</time></p>
+    <footer className="text-center text-sm text-newmo-300" role="contentinfo">
+      <div className="footer-content">
+        <ul className="flex items-center justify-between max-w-[100px] mx-auto">
+          <li>
+            <a
+              href="https://twitter.com/ikmnjrd"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+              title="Twitter"
+              className="hover:opacity-50 block"
+            >
+              <SvgIcon type="twitter" size="2.3em" strokeWidth={2.3} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/ikmnjrd"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+              title="GitHub"
+              className="hover:opacity-50 block"
+            >
+              <SvgIcon type="github" size="2em" strokeWidth={1} />
+            </a>
+          </li>
+        </ul>
+        <p>©︎ikmnjrd - <time dateTime={date.toString()}>{ date.getFullYear() }</time></p>
+      </div>
+      <div className="wave"></div>
+      <div className="wave"></div>
+      {/* <div className="wave-bottom"></div> */}
+      
     </footer>
   )
 }
