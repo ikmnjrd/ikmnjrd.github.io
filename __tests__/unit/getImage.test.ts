ba8f4e1086f51408cc1fcdf120404b0549a6940c
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-09-19 17:31:31 +0900

    test: 画像のfetchのテストの雛形追加

---
+++ __tests__/unit/getImage.test.ts
@@ -0,0 +1,45 @@
+import {
+  getImageFromWeb,
+  type ImageConverterProps,
+} from '~/utils/getImageFromWeb'
+
+jest.mock('~/utils/getImageFromWeb')
+
+const mockGetImageFromWeb =
+  getImageFromWeb as jest.MockedFunction<
+    typeof getImageFromWeb
+  >
+
+describe('getImageFromWeb', () => {
+  const props: ImageConverterProps = {
+    url: 'https://i.gyazo.com/mock-1ef68f7e017d8ed2309a6ff90d9036ad.png',
+    index: 0,
+    name: 'consolelog-effect',
+  }
+
+  afterEach(() => {
+    mockGetImageFromWeb.mockClear()
+  })
+
+  test('should be defined', () => {
+    expect(getImageFromWeb).toBeDefined()
+  })
+
+  test('should be called a time', async () => {
+    await mockGetImageFromWeb(props)
+
+    expect(
+      mockGetImageFromWeb
+    ).toHaveBeenCalledTimes(1)
+  })
+
+  // test('makes correct call', async () => {
+  //   const fetched = await getImageFromWeb(props)
+  //   expect(fetched).toBe('')
+  // })
+
+  // test('returns correct data', () => {
+  //   mockedFetch = getMockedFetch()
+  //   // ...
+  // })
+})
