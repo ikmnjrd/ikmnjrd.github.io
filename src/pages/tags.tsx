9cec01f3992771152cc663a5da2c08d32b1404a9
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-10-12 20:13:23 +0900

    feat: replace tags display

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,22 +1,82 @@
-import Link from 'next/link'
 import getTags, {
   ITagInfo,
 } from '~/utils/getTags'
 import TitleHead from '~/components/TitleHead'
+import SankeyChart from '~/components/SankeyChart'
+
+export interface Node {
+  name: string // tag
+  id: number // tag name
+  url: string
+}
+export interface ILink {
+  source: number | string // slug
+  target: number | string // tag name
+  value: number
+}
 
 export default function Tag({
+  wholeTags,
   countedTags,
+  filesData,
 }: ITagInfo) {
-  const tags_obj = Object.entries(
-    countedTags
-  ).map((tag) => {
-    return (
-      <li key={tag[0]}>
-        <Link href={`/tag/${tag[0]}`}>
-          <a className="hover:underline hover:text-newmo-400 visited:text-newmo-300">{`${tag[0]}(${tag[1]})`}</a>
-        </Link>
-      </li>
-    )
+  const baseTagUrl = '/tag/'
+  const baseBlogUrl = '/blog/'
+
+  const NodesFactory = (data: ITagInfo) => {
+    const arr: Node[] = []
+    let idx = 0
+    for (const [key, _] of Object.entries(
+      data.countedTags
+    )) {
+      arr.push({
+        name: key,
+        id: idx,
+        url: baseTagUrl + key,
+      })
+      idx++
+    }
+    return arr
+  }
+
+  const Nodes = NodesFactory({
+    wholeTags,
+    countedTags,
+    filesData,
+  })
+  const lastNodesLength = Nodes.length
+
+  const linksFactory = (
+    data: ITagInfo
+  ): ILink[] => {
+    // 一時的に追加.あとで削除
+    data.wholeTags.forEach((i, idx) => {
+      Nodes.push({
+        name: i.slug,
+        id: lastNodesLength + idx,
+        url: baseBlogUrl + i.slug,
+      })
+    })
+
+    return data.wholeTags.map((i) => {
+      Nodes.findIndex((n) => n.name === i.tag)
+
+      return {
+        source: Nodes.findIndex(
+          (n) => n.name === i.tag
+        ),
+        target: Nodes.findIndex(
+          (n) => n.name === i.slug
+        ),
+        value: 1,
+      }
+    })
+  }
+
+  const Links = linksFactory({
+    wholeTags,
+    countedTags,
+    filesData,
   })
 
   return (
@@ -25,19 +85,24 @@ export default function Tag({
       <h1 className="text-3xl pt-8 pb-4">
         Tag一覧
       </h1>
-      <div>{tags_obj}</div>
+      <SankeyChart
+        nodes={Nodes}
+        links={Links}
+        lastIndex={lastNodesLength}
+      />
     </>
   )
 }
 
 export async function getStaticProps() {
-  const { wholeTags, countedTags } =
+  const { wholeTags, countedTags, filesData } =
     await getTags()
 
   return {
     props: {
       wholeTags,
       countedTags,
+      filesData,
     },
   }
 }
