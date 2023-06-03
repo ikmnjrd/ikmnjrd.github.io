8c4534a48a6a7c82d8ad7858a39a993b88fe65c4
ike <40803799+ikmnjrd@users.noreply.github.com>
2023-06-04 00:38:52 +0900

    feat: ページネーションコンポーネント化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import Post from '~/components/Post'
 import getPostsMeta from '~/utils/getPostsMeta'
 import TitleHead from '~/components/TitleHead'
 import OgpHead from '~/components/OgpHead'
+import { Pagination } from '~/components/Pagination'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
 import { IFrontMatter } from '~/utils/getPostFilesData'
 import { generateRssFeed } from '~/scripts/rss'
 import { generateSitemap } from '~/scripts/generate-sitemap'
@@ -43,22 +43,12 @@ export default function Home({
           />
         ))}
       </ul>
-      <div className="flex justify-between">
-        <div>
-          {pageIndex > 0 && (
-            <span className="mx-2">
-              <Link href={`?p=${pageIndex - 1}`}>前へ</Link>
-            </span>
-          )}
-        </div>
-        <div>
-          {pageIndex < posts.length - 1 && (
-            <span className="mx-2">
-              <Link href={`?p=${pageIndex + 1}`}>次へ</Link>
-            </span>
-          )}
-        </div>
-      </div>
+
+      <Pagination
+        currentPageIndex={pageIndex}
+        targetArray={posts}
+        style={{ marginLeft: 'auto' }}
+      />
     </>
   )
 }
