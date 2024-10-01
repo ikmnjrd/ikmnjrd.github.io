c4b3325df6516597b9738ef689a67a68b6de9358
ike <40803799+ikmnjrd@users.noreply.github.com>
2024-10-02 02:22:03 +0900

    feat: 記事の目次リンクとタグリンクの追加

---
+++ src/components/Outline.tsx
@@ -0,0 +1,31 @@
+import Link from '~/components/Link'
+import type { HTMLProps } from 'react'
+
+export function Outline({
+  contents,
+  ...props
+}: {
+  contents: { text: string; link: string }[]
+} & HTMLProps<HTMLDivElement>) {
+  return (
+    <div {...props}>
+      <div className="hidden lg:block">
+        {contents.map(({ text, link }) => (
+          <div key={text} className="mb-2">
+            <Link href={`#${link}`}>{text}</Link>
+          </div>
+        ))}
+      </div>
+      <details className="lg:hidden">
+        <summary className="mb-2">目次</summary>
+        <ul>
+          {contents.map(({ text, link }) => (
+            <li key={text}>
+              <Link href={`#${link}`}>{text}</Link>
+            </li>
+          ))}
+        </ul>
+      </details>
+    </div>
+  )
+}
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -10,23 +10,31 @@ import OgpHead from '~/components/OgpHead'
 import markdownStyles from '~/components/markdown/markdown-styles.module.css'
 import { optimizeImages } from '~/utils/ImageOptimizer'
 import 'prism-themes/themes/prism-nord.min.css'
+import { Outline } from '~/components/Outline'
+import Link from 'next/link'
+import styles from './article.module.css'
 
 export interface IBlog {
   frontmatter: {
     title: string
     date: string
+    tag?: string[]
     cover_image?: string
     description?: string
   }
   slug: string
   innerHtml: string
+  anchors: { text: string; link: string }[]
 }
 
 export default function PostPage({
-  frontmatter: { title, date, cover_image, description },
+  frontmatter: { title, date, cover_image, description, tag },
   slug,
   innerHtml,
+  anchors,
 }: IBlog) {
+  const dotBorder = 'border-dotted border-newmo-400'
+
   return (
     <>
       <TitleHead title={title} />
@@ -42,18 +50,65 @@ export default function PostPage({
         )}
       </Head>
 
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
-        {title}
-      </h1>
-      <div className="text-right">Posted on {date}</div>
-      <img src={cover_image} alt="" />
+      <article
+        className={[
+          styles.article,
+          'lg:grid ',
+          'px-2 md:px-0',
+        ].join(' ')}
+      >
+        <div className="pb-4">
+          <h1 className={`${styles.title} mb-4`}>{title}</h1>
 
-      <div
-        dangerouslySetInnerHTML={{
-          __html: innerHtml,
-        }}
-        className={markdownStyles['markdown']}
-      ></div>
+          <ul className="mb-2">
+            {tag &&
+              tag.map((t) => (
+                <li key={t} className="mr-2 inline-block">
+                  <Link
+                    href={`/tag/${t}`}
+                    className="border-newmo-400 border-2 px-4 py-1 rounded-full"
+                  >
+                    {t}
+                  </Link>
+                </li>
+              ))}
+          </ul>
+
+          <div className="">Posted on {date}</div>
+        </div>
+
+        {/* TODO: 記事内で使われているかどうか調査の後、削除 */}
+        <img src={cover_image} alt="" />
+
+        <Outline
+          contents={anchors}
+          style={{
+            maxWidth: 'auto',
+            order: 2,
+            padding: '1rem 2rem',
+          }}
+          className={[
+            styles.gridElSticky,
+            dotBorder,
+            'border-t-2',
+          ].join(' ')}
+        />
+
+        <div
+          dangerouslySetInnerHTML={{
+            __html: innerHtml,
+          }}
+          className={[
+            markdownStyles['markdown'],
+            dotBorder,
+            'border-t-2',
+            'md:border-r-2 md:pr-4',
+          ].join(' ')}
+          style={{
+            order: 1,
+          }}
+        ></div>
+      </article>
     </>
   )
 }
@@ -98,6 +153,8 @@ export async function getStaticProps({
   const { data: frontmatter, content } = matter(markdownWithMeta)
   const innerHtml = markdownIt.render(content)
 
+  console.log({ frontmatter })
+
   const image_tags: string[] =
     innerHtml.match(/src="https:\/\/i\.gyazo.*?"/g) ?? []
 
@@ -115,11 +172,21 @@ export async function getStaticProps({
     )
   }
 
+  const ids = replaceHtml.match(/id=["'].+?["']/g)
+  console.log({ ids })
+  const anchors =
+    ids?.map((id) => ({
+      text: decodeURI(id.replace(/id=["'](.+)["']/, '$1')),
+      link: id.replace(/id=["'](.+)["']/, '$1'),
+    })) ?? []
+  console.log({ anchors })
+
   return {
     props: {
       frontmatter,
       slug,
       innerHtml: replaceHtml,
+      anchors,
     },
   }
 }
