af62442d7126ac288344114ef2003dde67c3d933
ike <40803799+ikmnjrd@users.noreply.github.com>
2025-01-01 13:25:57 +0900

    feat: 検索をalgoliaからMiniSearchに切り替え

---
+++ src/scripts/search-json.ts
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import getPostFilesData from '~/utils/getPostFilesData'
+
+export const generateJsonForSearch = async () => {
+  const posts = await getPostFilesData()
+  const postsWithId = posts.map((post, index) => ({
+    ...post,
+    id: index,
+  }))
+
+  fs.writeFileSync(
+    './public/to-search/blog-contents.json',
+    JSON.stringify(postsWithId)
+  )
+}
+
+generateJsonForSearch()
